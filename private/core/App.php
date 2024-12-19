d86d53e63f2587186c347963023496ec61f87540
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2024-12-19 18:20:15 +0200

    Created a function to get the sanitized and trimed url and called it from the constructor. Also included parameters.

--- private/core/App.php
+++ private/core/App.php
@@ -1,1 +1,21 @@
-App.pp
+<?php
+/**
+ * Author: Oamogetswe Mgidi
+ *
+ * Main app file
+ */
+class App
+{
+    protected $controller = 'HomeController';
+    protected $method = 'index';
+    protected $params = [];
+    public function __construct()
+    {
+        print_r($this->getURL());
+    }
+    private function getURL()
+    {
+        echo '<pre>';
+        return explode('/', filter_var(trim($_GET['url'], '/')), FILTER_SANITIZE_URL);
+    }
+}
