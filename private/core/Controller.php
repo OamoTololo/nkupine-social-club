ccd5ef8fd6e7df9cb81ef039da58c57248fe9ed3
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2024-12-30 16:00:08 +0200

    Created a view function to get the file contents of the views directory and if it can't it should display the 404 php file.

--- private/core/Controller.php
+++ private/core/Controller.php
@@ -8,5 +8,14 @@
 
 class Controller
 {
+    public function view($view, $data = [])
+    {
+        extract($data);
 
+        if (file_exists("./private/views/" .$view . ".php")) {
+            return file_get_contents("./private/views/" .$view . ".php");
+        } else {
+            return file_get_contents("./private/views/404.php");
+        }
+    }
 }
