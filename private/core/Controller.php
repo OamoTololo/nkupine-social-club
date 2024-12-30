508a4c77918108dbb99e48925a031f8713ac547c
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2024-12-30 13:33:20 +0200

    Created a controller class that will have all the same functionality that will be used by the apps controllers.

---
+++ private/core/Controller.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of the Nkupine Social Club Application.
+ *
+ * @author      Oamogetswe Mgidi
+ * @copyright   Copyright (c) Ntwana Sosha LTD
+ */
+
+class Controller
+{
+
+}
