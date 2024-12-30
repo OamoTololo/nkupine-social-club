bce64d8787b030be151345685dd044927abc5eca
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2024-12-31 01:02:56 +0200

    Created a header and footer file with it's logic.

---
+++ private/views/includes/Footer.php
@@ -0,0 +1,10 @@
+</div>
+
+<script src="<?=ROOT?>/assets/js/jquery.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="<?=ROOT?>/assets/js/bootstrap.min.js"></script>
+<script src="<?=ROOT?>/assets/js/all.min.js"></script>
+<script src="<?=ROOT?>/assets/js/nkupine-social-club.js"></script>
+
+</body>
+</html>
---
+++ private/views/includes/Header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nkupine</title>
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/all.min.css">
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=ROOT?>/assets/css/nkupine-social-club.css">
+</head>
+<body>
+<div style="min-width: 350px;">
