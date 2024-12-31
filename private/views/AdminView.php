005345bf6b056c890a8cc5a73f8c5a9a46197e7f
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2024-12-31 02:32:37 +0200

    Modified the header part of the admin file.

--- private/views/AdminView.php
+++ private/views/AdminView.php
@@ -1,13 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin</title>
-</head>
-<body>
-
-</body>
-</html>
+<?php
+$this->view('includes/Header');
+?>
+    <div class="container-fluid">
+        <h1><i class="fa-solid fa-plus"></i> Admin</h1>
+    </div>
+<?php
+$this->view('includes/Footer');
+?>
