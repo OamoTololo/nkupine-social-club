4734ce46a5a27f904670122a8385a70679f64336
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2024-12-31 02:33:43 +0200

    Modified the header part of the Member view file.

--- private/views/MemberView.php
+++ private/views/MemberView.php
@@ -1,13 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Member</title>
-</head>
-<body>
+<?php
+$this->view('includes/Header');
+?>
+    <div class="container-fluid">
+        <h1><i class="fa-solid fa-plus"></i> Member</h1>
+    </div>
 
-</body>
-</html>
+<?php
+$this->view('includes/Footer');
+?>
