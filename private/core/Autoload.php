92b5a64158ec965594f88b7b65d84c92b81814d2
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2025-01-06 22:37:29 +0200

    Modified the database required file to be above the model file.

--- private/core/Autoload.php
+++ private/core/Autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 require 'Config.php';
-require 'Controller.php';
-require 'Modal.php';
-require 'App.php';
 require 'NkupineSocialClubDB.php';
+require 'Controller.php';
+require 'Model.php';
+require 'App.php';
