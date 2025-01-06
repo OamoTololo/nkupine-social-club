9fcc594c9fb47ad2d1c4c33ec8af73c6e7e9384f
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2025-01-06 22:40:33 +0200

    Modified the where function logic and added a find all function to read everything from a table.

--- private/core/Model.php
+++ private/core/Model.php
@@ -6,17 +6,32 @@
  * @copyright   Copyright (c) Ntwana Sosha LTD
  */
 
-class Modal extends NkupineSocialClubDB
+class Model extends NkupineSocialClubDB
 {
     protected $table = "Members";
 
     public function where($column, $value)
     {
-        $query = "SELECT * FROM {$this->table} WHERE $column = :value";
-        echo $query;
+        $column = addslashes($column);
+        $query = "SELECT * FROM $this->table WHERE $column = :value";
 
         try {
-            return $this->query($query, ['value' => $value]);
+            return $this->query($query, [
+                'value' => $value
+            ]);
+        } catch (PDOException $e) {
+            error_log("Query failed: $query");
+            error_log("Error: " . $e->getMessage());
+            return false;
+        }
+    }
+
+    public function findAll()
+    {
+        $query = "SELECT * FROM $this->table";
+
+        try {
+            return $this->query($query);
         } catch (PDOException $e) {
             error_log("Query failed: $query");
             error_log("Error: " . $e->getMessage());
