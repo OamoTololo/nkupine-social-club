650d84aefaff6bfaa0dabc8bd232b1d9d5f1169e
Oamogetswe Mgidi <51949380+OamoTololo@users.noreply.github.com>
2025-01-06 22:45:16 +0200

    Added classes elated to the database tables.

---
+++ private/models/Contributions.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of the Nkupine Social Club Application.
+ *
+ * @author      Oamogetswe Mgidi
+ * @copyright   Copyright (c) Ntwana Sosha LTD
+ */
+namespace modals;
+
+class Contributions extends \Model
+{
+
+}
---
+++ private/models/Loans.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * This file is part of the Nkupine Social Club Application.
+ *
+ * @author      Oamogetswe Mgidi
+ * @copyright   Copyright (c) Ntwana Sosha LTD
+ */
+namespace modals;
+
+use Model;
+
+class Loans extends Model
+{
+
+}
